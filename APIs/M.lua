5d3ea76ff661700930d7bffaaf59af0a435f6aa4
RealTTJY <icegen168@gmail.com>
2026-08-02 13:31:07 +0700

    Update M.lua

--- APIs/M.lua
+++ APIs/M.lua
@@ -12106,7 +12106,7 @@ local FreeLoad, KeyLoad = {
     };
     [2294168059] = {
         File = "2294168059";
-        Version = "TheMimicV3.B4";
+        Version = "TheMimicV3.B5";
         Included = {"CorePackage", "LoadUILib", "IntroLib", "Windy", "ClientPackage", "CoruTask", "CommonF", "ESPPackage", "PromptPackage"};
     };
 };
