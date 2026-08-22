0480b86c44fc0f5a078c73581eafb13ecd2b82df
RealTTJY <icegen168@gmail.com>
2026-08-22 17:09:43 +0700

    Evade_V3.50

--- APIs/M.lua
+++ APIs/M.lua
@@ -12269,7 +12269,7 @@ local FreeLoad, KeyLoad = {
     };
     [3647333358] = {
         File = "3647333358";
-        Version = "Evade_V3.49";
+        Version = "Evade_V3.50";
         Included = {"CorePackage", "LoadUILib", "IntroLib", "Windy", "ClientPackage", "CoruTask", "CommonF", "ESPPackage"};
     };
     [2294168059] = {
