87fdf3a33e947fbf8ec9f3ad6b0432f76308dc31
RealTTJY <icegen168@gmail.com>
2026-08-28 21:55:44 +0700

    DA_V3.70

--- APIs/M.lua
+++ APIs/M.lua
@@ -12305,7 +12305,7 @@ local FreeLoad, KeyLoad = {
 }, {
     [1235188606] = {
         File = "1235188606";
-        Version = "DA_V3.69";
+        Version = "DA_V3.70";
         Included = {"CorePackage", "LoadUILib", "IntroLib", "Windy", "ClientPackage", "CoruTask", "CommonF", "ESPPackage"};
     };
     [3647333358] = {
