125a5be129aad8d6c72ca7d54b4b8ff3d43891c3
RealTTJY <icegen168@gmail.com>
2026-09-07 21:27:44 +0700

    Update M.lua

--- APIs/M.lua
+++ APIs/M.lua
@@ -12319,7 +12319,7 @@ local FreeLoad, KeyLoad = {
 }, {
     [1235188606] = {
         File = "1235188606";
-        Version = "DA_V3.75";
+        Version = "DA_V3.77";
         Included = {"CorePackage", "LoadUILib", "IntroLib", "Windy", "ClientPackage", "CoruTask", "CommonF", "ESPPackage"};
     };
     [3647333358] = {
